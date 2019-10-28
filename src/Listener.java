348e1324cf3d87bb9a3999672488773e809bbd3c
Anubhav <44627486+sirdonutsiiv@users.noreply.github.com>
2019-10-28 13:33:49 -0500

    Create Listener.java

---
+++ src/Listener.java
@@ -0,0 +1,89 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Listeners extends JFrame implements Runnable, KeyListener, MouseListener {
+    
+    
+    public Listeners() {
+        Server server = new Server();
+//        this.setSize(100, 100);
+//        this.setLocationRelativeTo(null);
+        this.setSize((int)Toolkit.getDefaultToolkit().getScreenSize().getWidth(), (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+        this.setUndecorated(true);
+        this.setBackground(new Color(0, 0, 0, 1));
+        this.setVisible(true);
+        this.addKeyListener(this);
+        this.addMouseListener(this);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Server.adrian.keyPressed = -2;
+    }
+    
+    @Override
+    public void run() {
+        Server server = new Server();
+        Server.adrian = new Adrian(Server.adrian.mouseLocation, -1);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+//         String key = e.getKeyText(e.getKeyCode());
+        
+//         if(key.equals("Delete"))
+//             System.exit(-1);
+//         adrian = new Adrian(adrian.getMouse(), e.getKeyCode());
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        Server server = new Server();
+//         adrian = new Adrian(adrian.getMouse(), -1);
+        String key = e.getKeyText(e.getKeyCode());
+        
+        if(key.equals("NumPad-7"))
+            System.exit(-1);
+        Server.adrian = new Adrian(server.adrian.mouseLocation, e.getKeyCode());
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        Server server = new Server();
+        //left-click will be stored in Adrian with a keyPressed = 1000
+        //right-click will be stored in Adrian with a keyPressed = 1001
+        //middle-click will be stored in Adrian with a keyPressed = 1002
+        
+        //use swingutilities to differentiate between the different clicks
+        if(e.getButton() == MouseEvent.BUTTON1) {
+            Server.adrian = new Adrian(server.adrian.mouseLocation, 1000); //left click
+        }
+        else if(e.getButton() == MouseEvent.BUTTON2) {
+            Server.adrian = new Adrian(server.adrian.mouseLocation, 1001); //right click
+        }
+        else if(e.getButton() == MouseEvent.BUTTON3) {
+            Server.adrian = new Adrian(server.adrian.mouseLocation, 1002); //middle click
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+}
